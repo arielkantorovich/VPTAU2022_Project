56e2b55991f232bf75ae8ca6b1933aa73a241ae4
Ariel Kantorovich <ubuntu@ARIELKA-LAP.EMIT.local>
2023-05-21 16:17:59 +0300

    Video stabilization using feature matching

--- Code/Video_Stabilization.py
+++ Code/Video_Stabilization.py
@@ -45,20 +45,56 @@ def fixBorder(frame: np.ndarray) -> np.ndarray:
     frame = cv2.warpAffine(frame, T, (w, h))
     return frame
 
+def Finding_Matching_points(prev_img, curr_img):
+    """
+    :param prev_img: ndarray (h, w)
+    :param curr_img: ndarray (h, w)
+    :return: (prev_pts, curr_pts) correspond key points
+    """
+    # Step1:  let's find interest points
+    fast = cv2.FastFeatureDetector_create()
+    kp_prev = fast.detect(prev_img, None)
+    kp_curr = fast.detect(curr_img, None)
+    # Step2: create BRIEF descriptor and describe interest points
+    brief = cv2.xfeatures2d.BriefDescriptorExtractor_create()
+    kp_prev, des_prev = brief.compute(prev_img, kp_prev)
+    kp_curr, des_curr = brief.compute(curr_img, kp_curr)
+    # Step3: Make feature matching using Brute Force Matcher
+    bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+    matches = bf.match(des_prev, des_curr)
+    # Step 4: Extract corresponding points
+    prev_pts = []
+    curr_pts = []
+    for match in matches:
+        prev_idx = match.queryIdx
+        curr_idx = match.trainIdx
+        prev_pt = kp_prev[prev_idx].pt
+        curr_pt = kp_curr[curr_idx].pt
+        prev_pts.append(prev_pt)
+        curr_pts.append(curr_pt)
+
+    # Convert points to NumPy arrays
+    prev_pts = np.array(prev_pts)
+    curr_pts = np.array(curr_pts)
+
+    return prev_pts, curr_pts
+
+print("START VIDEO STABILIZATION PROCESS........")
+
 ###############################################################################################################################
 # Initialize parameters
 # FILL IN YOUR ID
 ID1 = '308345891'
 ID2 = '211670849'
-input_video_name = 'INPUT.avi'
-output_video_name = f'{ID1}_{ID2}_stabilized_video.avi'
+input_video_name = '../Inputs/INPUT.avi'
+output_video_name = f'../Outputs/{ID1}_{ID2}_stabilized_video.avi'
 
 ##################### Step 1 : Set Input and Output Videos #######################
 # Read input video
 cap = cv2.VideoCapture(input_video_name)
 # Get frame count
-n_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 params = get_video_parameters(cap)
+n_frames = params["frame_count"]
 out = cv2.VideoWriter(output_video_name, fourcc=cv2.VideoWriter_fourcc(*'XVID'), fps=params["fps"],
                       frameSize=(params["width"], params["height"]), isColor=True)
 
@@ -71,39 +107,20 @@ prev_gray = cv2.cvtColor(prev, cv2.COLOR_BGR2GRAY)
 ######################## Step 3: Find motion between frames  #####################################
 # Pre-define transformation-store array
 transforms = np.zeros((n_frames - 1, 9), np.float32)
-
-lk_params = dict(winSize=(15, 15),
-                  maxLevel=5,
-                  criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
-
-
 for i in range(n_frames):
     # Detect feature points in previous frame
-    prev_pts = cv2.goodFeaturesToTrack(prev_gray,
-                                       maxCorners=100,
-                                       qualityLevel=0.3,
-                                       minDistance=7,
-                                       blockSize=7)
     # Read next frame
     success, curr = cap.read()
     if not success:
         break
     # Convert to grayscale
     curr_gray = cv2.cvtColor(curr, cv2.COLOR_BGR2GRAY)
-    # Calculate optical flow (i.e. track feature points)
-    curr_pts, status, err = cv2.calcOpticalFlowPyrLK(prev_gray, curr_gray, prev_pts, None, **lk_params)
-    # Sanity check
-    assert prev_pts.shape == curr_pts.shape
-    # Filter only valid points
-    idx = np.where(status == 1)[0]
-    prev_pts = prev_pts[idx]
-    curr_pts = curr_pts[idx]
-    # Find transformation matrix
+    prev_pts, curr_pts = Finding_Matching_points(prev_img=prev_gray, curr_img=curr_gray)
+
+    # Find Homography transformation matrix
     H, _ = cv2.findHomography(prev_pts, curr_pts, method=cv2.RANSAC, ransacReprojThreshold=5.0)
     transforms[i] = H.flatten()
     prev_gray = curr_gray
-    print("Frame: " + str(i) + "/" + str(n_frames) + " -  Tracked points : " + str(len(prev_pts)))
-
 
 ###################################### Step 4: Calculate smooth motion between frames ###############################
 # Compute trajectory using cumulative sum of transformations
@@ -136,7 +153,10 @@ for i in range(n_frames-1):
 # Save same size of frames
 out.write(frame_stabilized)
 
+
 # Clear all capture
 cap.release()
 out.release()
 cv2.destroyAllWindows()
+
+print("FINISH VIDEO STABILIZATION PROCESS........")
--- Code/main.py
+++ Code/main.py
@@ -1,34 +1,6 @@
-def my_lucas_kanade_step(I1: np.ndarray,
-                      I2: np.ndarray,
-                      window_size: int) -> tuple[np.ndarray, np.ndarray]:
-    # Initialize step
-    h, w, c = I1.shape
-    I1 = I1.astype(np.float32)
-    I2 = I2.astype(np.float32)
-    du = np.zeros((h, w))
-    dv = np.zeros((h, w))
-    epsilon = 1e-4
-    # First Let's calculate Ix, Iy, It u
-    I2_shift_x = np.zeros_like(I2)
-    I2_shift_y = np.zeros_like(I2)
-    I2_shift_x[:, 1:, :] = I2[:, 0:-1, :]
-    I2_shift_y[1:, :, :] = I2[0:-1, :, :]
-    Ix = I2_shift_x - I2
-    Iy = I2_shift_y - I2
-    It = I2 - I1
-    # Prepare the data for vectorization use that the channel is RGB
-    Ixx = np.sum(Ix * Ix, axis=2)
-    Iyy = np.sum(Iy * Iy, axis=2)
-    Ixy = np.sum(Ix * Iy, axis=2)
-    Ixt = np.sum(-Ix * It, axis=2)
-    Iyt = np.sum(-Iy * It, axis=2)
-    # Step 3:
-    # Compute du and dv without for-loops
-    ATA = np.stack((Ixx, Ixy, Ixy, Iyy), axis=1).reshape(h, w, 2, 2)
-    ATb = np.stack((Ixt, Iyt), axis=1).reshape(h, w, 2, 1)
-    det_ATA = ATA[:, :, 0, 0] * ATA[:, :, 1, 1] - ATA[:, :, 0, 1] * ATA[:, :, 1, 0]
-    valid_idx = det_ATA > epsilon
-    U_V_LS = np.zeros((h, w, 2, 1))
-    U_V_LS[valid_idx] = np.linalg.inv(ATA[valid_idx]) @ ATb[valid_idx]
-    du, dv = U_V_LS[:, :, 0, 0], U_V_LS[:, :, 1, 0]
-    return du, dv
+import os
+
+# Set the working directory
+os.chdir('Code')
+# Call Video_Stabilization.py
+os.system('python Video_Stabilization.py')
